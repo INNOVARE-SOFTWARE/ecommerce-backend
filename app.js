3fec55f9f1a42fc8d3b84d65e921e768299fcfde
Marcelo Agustini <marceloagustini@iMac-de-Marcelo.local>
2023-11-06 17:27:39 -0300

    change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = 3001;
 const mongodb = "mongodb://localhost:27017/nodejs-db"
 var cors = require('cors');
 
